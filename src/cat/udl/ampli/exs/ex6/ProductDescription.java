cf0bada8d29339057cb6c6c8f6eb2aae7529a808
Oriol Alàs Cercós <oriolalascercos@gmail.com>
2020-05-01 13:29:13 +0200

    Ex 6 is done

---
+++ src/cat/udl/ampli/exs/ex6/ProductDescription.java
@@ -0,0 +1,15 @@
+package cat.udl.ampli.exs.ex6;
+
+public class ProductDescription {
+    private String description;
+    private int price;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+}
---
+++ src/cat/udl/ampli/exs/ex6/Store.java
@@ -0,0 +1,4 @@
+package cat.udl.ampli.exs.ex6;
+
+public class Store {
+}
